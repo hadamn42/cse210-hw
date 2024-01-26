a4c2fa426435edbd9463ecc3e6dba02c46ac53c3
hadamn42 <an.humes@gmail.com>
2024-01-25 19:49:21 -0700

    completed Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction friction = new Fraction();
+        Console.WriteLine(friction.GetFractionString());
+        Console.WriteLine(friction.GetDecimalValue());
+        
+        Fraction froctions = new Fraction(5);
+        Console.WriteLine(froctions.GetFractionString());
+        Console.WriteLine(froctions.GetDecimalValue());
+
+        Fraction fruction = new Fraction(3, 4);
+        Console.WriteLine(fruction.GetFractionString());
+        Console.WriteLine(fruction.GetDecimalValue());
+
     }
 }
