ed6b1784f89be6ee747365f6e8cdf96dae139bcd
hadamn42 <an.humes@gmail.com>
2024-02-07 19:00:14 -0700

    Completed Learning05.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square squig = new Square("green", 4);
+        Rectangle jeff = new Rectangle("blue", 2, 4);
+        Circle greg = new Circle("black", 4);
+
+        shapes.Add(squig);
+        shapes.Add(jeff);
+        shapes.Add(greg);
+
+        foreach (Shape shape in shapes){
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"The color is {color} and the area is {area}");
+        }
+
+
     }
 }
